c8381337166758372e61a6a2459ac26a596aa04a
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2016-06-24 17:47:03 +0200

    Update readme

--- app/modules/ModuleConfig.scala
+++ app/modules/ModuleConfig.scala
@@ -6,7 +6,6 @@ import com.typesafe.config.Config
 
 @Singleton
 class ModuleConfig {
-
   def loadConfig() : Config = {
     val config = com.typesafe.config.ConfigFactory.parseFile(new java.io.File("/var/novapost/showcase/showcase.yml"))
     if(config.isEmpty) {
@@ -16,7 +15,5 @@ class ModuleConfig {
     }
     config
   }
-
   loadConfig()
-
 }
